828f0d809ad5c7b5fcc57bc79d599667e98ebad9
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2024-10-18 18:50:22 -0300

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
       },
+      experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
